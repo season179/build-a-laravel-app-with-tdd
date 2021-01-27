3e315cdd10464e76d72fad8a882bc086fc9bb4fa
season <season.saw@gmail.com>
2021-01-27 18:56:13 +0800

    Completed 'Grid Spacing and Card Tweaks'.

--- src/resources/views/projects/index.blade.php
+++ src/resources/views/projects/index.blade.php
@@ -1,20 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="flex items-center">
-    <a href="/projects/create">New Project</a>
-  </div>
+  <header class="flex items-center mb-3 py-4">
+    <div class="flex justify-between items-center w-full">
+      <h2 class="text-gray-500">My Projects</h2>
+      <a href="/projects/create" class="button">New Project</a>
+    </div>
+  </header>
 
-  <div class="flex">
+  <main class="lg:flex lg:flex-wrap -mx-3">
     @forelse ($projects as $project)
-      <div class="bg-white mr-4 rounded shadow w-1/3 p-5" style="height: 200px">
-        <h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
+      <div class="lg:w-1/3 px-3 pb-6">
+        <div class="bg-white p-5 rounded-lg shadow" style="height: 200px">
+          <h3 class="font-normal text-xl py-4 -ml-5 mb-3 border-l-4 border-blue-400 pl-4">
+            <a href="{{ $project->path() }}">{{ $project->title }}</a>
+          </h3>
 
-        <div class="text-gray-400">{{ Illuminate\Support\Str::limit($project->description, $limit=100) }}</div>
-      </div>    
+          <div class="text-gray-400">{{ Illuminate\Support\Str::limit($project->description, $limit=100) }}</div>
+        </div>    
+      </div>
     @empty
       <div>No projects yet.</div>
     @endforelse
-    
-  </div>
+  </main>
 @endsection
