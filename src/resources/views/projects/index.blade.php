a555e3fbd648d45ce9dc33969215992231519b39
season <season.saw@gmail.com>
2021-01-27 18:11:32 +0800

    Completed 'Go Go Gadget Tailwind'.

--- src/resources/views/projects/index.blade.php
+++ src/resources/views/projects/index.blade.php
@@ -4,13 +4,17 @@
   <div class="flex items-center">
     <a href="/projects/create">New Project</a>
   </div>
-  
 
-  <ul>
+  <div class="flex">
     @forelse ($projects as $project)
-      <li><a href="{{ $project->path() }}">{{ $project->title }}</a></li>
+      <div class="bg-white mr-4 rounded shadow w-1/3 p-5" style="height: 200px">
+        <h3 class="font-normal text-xl py-4">{{ $project->title }}</h3>
+
+        <div class="text-gray-400">{{ Illuminate\Support\Str::limit($project->description, $limit=100) }}</div>
+      </div>    
     @empty
-      <li>No projects yet.</li>
+      <div>No projects yet.</div>
     @endforelse
-  </ul>  
+    
+  </div>
 @endsection
